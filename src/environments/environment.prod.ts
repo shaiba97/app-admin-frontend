2bf8575a7accd01a9642067e62511fca996293e8
shaiba97 <hsnshaiba17@gmail.com>
2026-07-29 08:06:16 +0530

    fix: update backend URL to app-backend-gz2l.onrender.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   apiUrl: {
-    admin:    'https://rihla-backend-rbh7.onrender.com/api',
-    company:  'https://rihla-backend-rbh7.onrender.com/api-company',
-    customer: 'https://rihla-backend-rbh7.onrender.com/api-customer',
+    admin:    'https://app-backend-gz2l.onrender.com/api',
+    company:  'https://app-backend-gz2l.onrender.com/api-company',
+    customer: 'https://app-backend-gz2l.onrender.com/api-customer',
   },
-  wsUrl: 'https://rihla-backend-rbh7.onrender.com',
+  wsUrl: 'https://app-backend-gz2l.onrender.com',
 };
